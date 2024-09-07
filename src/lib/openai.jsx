68b01a179b8144a6d157f48e9ecff6fa85a4e199
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-07 15:06:26 +0900

    apiの取得

--- src/lib/openai.jsx
+++ src/lib/openai.jsx
@@ -1,5 +1,27 @@
+import axios from "axios";
+
 class OpenaiClient {
-    constructor(){
+    constructor() {
         this.apiKey = process.env.REACT_APP_OPENAI_API_KEY;
     }
+
+    async completion(messages) {
+        const requestData = {
+            model: "gpt-4o-mini-2024-07-18",
+            messages,
+        };
+
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', requestData,
+            {
+                headers: {
+                    "Content-Type": 'application/json',
+                    Authorization: `Bearer ${this.apiKey}`,
+                },
+            }
+        );
+        return response.data.choices[0].message.content;
+    }
 }
+
+const openai = new OpenaiClient();
+export default openai;
