3ea89f14e4c2c0bad1787cabb7d2e840804a4a52
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-09 00:30:12 +0900

    markdownのついか

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import openai from "./lib/openai";
+import Markdown from "react-markdown";
 
 const prompt =
   `あなたは30年以上キャリアのあるエンジニアです。
@@ -11,11 +12,13 @@ const prompt =
   `
 
 function App() {
-  const [content, setContent]= useState("");
-  const [result, setResult]= useState("");
+  const [content, setContent] = useState("");
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const review = async () =>{
-    const messages =[
+  const review = async () => {
+    setIsLoading(true);
+    const messages = [
       {
         role: 'user',
         content: prompt + content,
@@ -23,6 +26,7 @@ function App() {
     ];
     const result = await openai.completion(messages);
     setResult(result);
+    setIsLoading(false)
   }
 
   return (
@@ -34,18 +38,22 @@ function App() {
         <div className="flex flex-col w-1/2 h-full bg-gray-900 overflow-y-auto">
           <div className="flex-1 p-4 text-white">
             <textarea
-            onChange={(e) =>
-              setContent(e.target.value)
-            }
-            className="h-full w-full bg-transparent text-white resize-none outline-none" />
+              onChange={(e) =>
+                setContent(e.target.value)
+              }
+              className="h-full w-full bg-transparent text-white resize-none outline-none" />
           </div>
-          <button onClick={review}
-           className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed">
-            レビューする
+          <button
+            onClick={review}
+            disabled={isLoading}
+            className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed">
+            {isLoading ? "レビュー中..." : "レビューする"}
           </button>
         </div>
         <div className="flex flex-col w-1/2 h-full items-center justify-center">
-          <div className="p-4 overflow-y-auto w-full">{result}</div>
+          <div className="p-4 overflow-y-auto w-full">
+            {isLoading ? ('レビュー中...') : (<Markdown className="markdown">{result}</Markdown>)}
+          </div>
         </div>
       </main>
     </div>
